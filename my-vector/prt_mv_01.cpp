bbb430e5d9ce0167964ac2f8a3d1ea7e9f146011
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2026-08-16 10:13:42 +0200

    Create prt_mv_01.cpp

---
+++ my-vector/prt_mv_01.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+class MyVector{
+    public:
+        int num_allocated;
+        int size;
+        int* data;
+
+        MyVector(){
+            size = 0;
+            num_allocated = 0;
+            data = nullptr;
+        }
+
+        void push_back(int value){
+            if()
+        }
+
+        void pop_back(){
+            
+        }
+
+};
+
+int main(){
+    return 0;
+}
